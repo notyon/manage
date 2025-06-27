aa140113fb998d404e5129528243d0963974f109
TuanMudaYonzz <143253401+notyon@users.noreply.github.com>
2025-06-27 19:20:41 +0700

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -27,3 +27,17 @@ def is_user_muted(chat_id: int, user_id: int) -> bool:
     """Cek apakah user masih muted"""
     data = mute_col.find_one({"chat_id": chat_id, "user_id": user_id})
     return bool(data and data.get("muted", False))
+
+# Koleksi welcome
+welcome_col = db['welcome']
+
+def set_welcome(chat_id: int, text: str):
+    welcome_col.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"text": text}},
+        upsert=True
+    )
+
+def get_welcome(chat_id: int) -> str:
+    data = welcome_col.find_one({"chat_id": chat_id})
+    return data["text"] if data else None
